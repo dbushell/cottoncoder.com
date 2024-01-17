94a8d2d823dbe951f8d49baea139d980068ef755
David Bushell <david@dbushell.com>
2024-01-17 20:31:02 +0000

    fix undefined headers

--- routes/(all)/index.js
+++ routes/(all)/index.js
@@ -7,6 +7,9 @@ export const order = 999;
 const themes = ['light', 'dark'];
 
 export const get = async (request, response, {platform}) => {
+  if (!(response instanceof Response)) {
+    return response;
+  }
   // Add strict security headers
   if (request.url.startsWith(Deno.env.get('ORIGIN'))) {
     response.headers.set(
@@ -15,10 +18,10 @@ export const get = async (request, response, {platform}) => {
     );
   }
   // Add policy to allow `data:` URIs in the stylesheet
-  if (response?.headers?.get('content-type')?.includes('text/html')) {
+  if (response.headers.get('content-type')?.includes('text/html')) {
     response.headers.append('x-img-src', 'data:');
     const theme = platform.cookies.get('theme')?.value;
-    if (theme) {
+    if (themes.includes(theme)) {
       let body = await response.text();
       body = body.replace(/<html([^>]+?)>/, `<html$1 data-theme="${theme}">`);
       response = new Response(body, response);
