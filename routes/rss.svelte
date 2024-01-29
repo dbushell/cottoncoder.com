0920737f4674cea73e1ad78d0a63d659b910f66b
David Bushell <david@dbushell.com>
2024-01-29 10:43:01 +0000

    cleanup rss

--- routes/rss.svelte
+++ routes/rss.svelte
@@ -7,7 +7,7 @@
 
   const template = `<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet href="/rss.xsl" type="text/xsl"?>
-<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title><![CDATA[{{meta.name}}]]></title>
     <description><![CDATA[{{meta.description}}]]></description>
